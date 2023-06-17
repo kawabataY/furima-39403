22f12edf16f46abc4d5b468413bbcb14acdc4d6d
kawabataY <yukikawabata07@gmail.com>
2023-06-17 21:13:50 +0900

    商品一覧表示の条件分岐機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :orders
+
   validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
